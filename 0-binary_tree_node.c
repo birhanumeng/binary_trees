7d7876a71bd3993046ad3e13a2a5b5b43ede52ae
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-13 05:53:53 -0800

    create a binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-
 	return (new);
 }
