9f18696f804ba4566ee208a41a59c9a5a99bc2f1
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-14 21:21:20 -0800

    find size of binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - finds the size of a binary tree
+ * @tree: refers to the root node
+ *
+ * Return: size of tree or, 0 it it is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t n = 0;
+
+	if (tree != NULL)
+	{
+		n += 1;
+		n += binary_tree_size(tree->left);
+		n += binary_tree_size(tree->right);
+	}
+	return (n);
+}
